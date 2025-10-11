ea2e5f00fc590b0b7d8961b9622cde3e0f8a349a
Eric Mena <menaericdaniel@gmail.com>
2025-10-11 01:30:27 -0300

    Implement product fetching and display in Home component; add loading state and filter products by category for rendering in ProductCard components.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
-import './Home.css'
+import React, { useState, useEffect } from "react";
+import "./Home.css";
+import { ProductCard } from "../../components/ProductCard";
 
 export const Home = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("/data/products.json")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setProducts(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error loading products:", error);
+        setLoading(false);
+      });
+  }, []);
+
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div className="container products-wrapper">
+        <div className="row">
+          <div className="col-12">
+            <h2 className="products-title">Últimos agregados</h2>
+          </div>
+          {loading ? (
+            <div className="alert alert-info">Cargando....</div>
+          ) : (
+            products
+              .filter((product) => product.category === "visited")
+              .map((product, index) => (
+                <ProductCard product={product} key={product.id + index} />
+              ))
+          )}
+        </div>
+      </div>
+      <div className="container products-wrapper">
+        <div className="row">
+          <div className="col-12">
+            <h2 className="products-title">Ofertas</h2>
+          </div>
+          {loading ? (
+            <div className="alert alert-info">Cargando....</div>
+          ) : (
+            products
+              .filter((product) => product.category === "in-sale")
+              .map((product, index) => (
+                <ProductCard product={product} key={product.id + index} />
+              ))
+          )}
+        </div>
+      </div>
+    </>
+  );
+};

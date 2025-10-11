5fd875b98cb5fbaf16d130db1ba2a05793e52bf6
Eric Mena <menaericdaniel@gmail.com>
2025-10-11 13:45:26 -0300

    Add logo SVG and enhance Header and Nav components; implement responsive layout with search functionality and navigation links.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,46 @@
 import React from 'react'
 import './Header.css'
-import { Nav } from '../Nav/Nav'
+import { Nav } from '../Nav'
+import { Link } from 'react-router-dom'
+import logo from '../../assets/logo-mercado-liebre.svg'
 
 export const Header = () => {
   return (
-    <>
+    <header className="main-header">
+    <div className="container">
+      <div className="row align-items-center">
+        <div className="col-5 col-md-2">
+          <Link to="/" className="main-header_home-link">
+            <img src={logo} alt="logo" />
+          </Link>
+        </div>
+
+        <div className="col-7 col-md-6">
+          <form
+            className="search-form"
+          >
+            <input
+              type="text"
+              name="keywords"
+              placeholder="Buscar productos, marcas y más"
+              className="search-form_input"
+            />
+            <button type="submit" className="search-form_button">
+              <i className="fas fa-search"></i>
+            </button>
+          </form>
+        </div>
+
+       
+      </div>
+
+      <button className="btn-toggle-navbar">
+        <i className="fas fa-bars"></i>
+      </button>
+
       <Nav/>
-      Header
-    </>
+    </div>
+  </header>
     
   )
 }
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
 import './Nav.css'
+import { Link, NavLink } from 'react-router-dom'
 
 export const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav className="main-navbar">
+    <ul className="left-navbar">
+    <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/products" end>Todos los productos</NavLink>
+      </li>
+      <li>
+        <NavLink to="/brands">Categorías</NavLink>
+      </li>
+      <li>
+        <NavLink to="/sell">
+          Vender
+        </NavLink>
+      </li>
+    </ul>
+    <ul className="right-navbar">
+      <li>
+        <span id="link-register">
+          Registrate <i className="fas fa-address-card"></i>
+        </span>
+      </li>
+      <li>
+        <Link to="/users/login">
+          Ingresá <i className="fas fa-sign-in-alt"></i>
+        </Link>
+      </li>
+    </ul>
+  </nav>
   )
 }

4e0e9965ee20f02606fc25828fd77f4718ce988d
Eric Mena <menaericdaniel@gmail.com>
2025-10-11 13:47:44 -0300

    Add routing for Products and ProductDetail pages in App component; integrate new routes for product listings and individual product details.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Home } from './pages/Home'
 import { Header } from './components/Header'
 import { Route, Routes } from 'react-router-dom'
 import { Footer } from './components/Footer/Footer'
+import { ProductDetail } from './pages/ProductDetail/ProductDetail'
+import { Products } from './pages/Products'
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/products' element={<Products/>}/>
+        <Route path='/products/:id' element={<ProductDetail/>}/>
       </Routes>
       <Footer/>
     </>

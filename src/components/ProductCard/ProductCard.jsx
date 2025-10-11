04c6ff6b51d2ed1eb7b1da9eb16f304f2d058890
Eric Mena <menaericdaniel@gmail.com>
2025-10-11 01:29:44 -0300

    Add ProductCard component with styling and export; includes layout for product details and image display.

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './ProductCard.css'
+import { Link } from 'react-router-dom'
+import { toThousand } from '../../helpers'
+
+export const ProductCard = ({ product }) => {
+  const { id, name, price, discount, image } = product
+
+  return (
+    <div className="col-12 col-sm-6 col-lg-3">
+    <section className="product-box">
+      <Link to={`/products/detail/${id}`}>
+        <figure className="product-box_image">
+          <img src={image} alt="imagen de producto" />
+        </figure>
+        <article className="product-box_data">
+          <h2>$ {toThousand(price)}</h2>
+          {discount ? <span>{discount}% OFF</span> : null}
+          <p>{name}</p>
+          <i className="fas fa-truck"></i>
+        </article>
+      </Link>
+    </section>
+  </div>
+  )
+}

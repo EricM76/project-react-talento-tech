4723c22cdc9ea204270b6e7fd1ab355e926cf4c3
Eric Mena <menaericdaniel@gmail.com>
2025-10-11 13:47:21 -0300

    Revamp Footer component with enhanced layout and content; include sections for payment options, free shipping, and security features, along with copyright information.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      <footer className="main-footer">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-lg-4">
+              <article className="footer-data">
+                <i className="fas fa-credit-card"></i>
+                <h2>Pagá con tarjeta o en efectivo</h2>
+                <p>
+                  Con Mercado Liebre Cash, tenés cuotas sin interés con tarjeta
+                  o efectivo en puntos de pago. ¡Y siempre es seguro!
+                </p>
+              </article>
+            </div>
+            <div className="col-12 col-lg-4">
+              <article className="footer-data">
+                <i className="fas fa-gift"></i>
+                <h2>Envío gratis desde $ 1.999</h2>
+                <p>
+                  Solo por estar registrado en Mercado Liebre tenés envíos
+                  gratis en miles de productos. Es un beneficio de Mercado
+                  Puntos.
+                </p>
+              </article>
+            </div>
+            <div className="col-12 col-lg-4">
+              <article className="footer-data">
+                <i className="fas fa-user-shield"></i>
+                <h2>Seguridad, de principio a fin</h2>
+                <p>
+                  ¿No te gusta? ¡Devolvelo! En Mercado Liebre, no hay nada que
+                  no puedas hacer, porque estás siempre protegido.
+                </p>
+              </article>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <div className="sub-footer">
+        <div className="container">
+          <p>Copyright © 2025 MercadoLiebre for Talento Digital by Eric.</p>
+        </div>
+      </div>
+    </>
+  );
+};

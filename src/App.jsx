177eccc5d649b3f6d3022ef70b94f9b3d096f3a5
@emena <menaericdaniel@gmail.com>
2025-11-11 10:55:12 -0300

    Refactor App component: integrate MainLayout and AdminLayout for improved routing structure, add AuthProvider for authentication context, and implement ProtectedRoute for admin dashboard access.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import './App.css'
 import { Home } from './pages/Home'
-import { Header } from './components/Header'
 import { Route, Routes } from 'react-router-dom'
-import { Footer } from './components/Footer/Footer'
 import { Categories } from './pages/Categories'
 import { NotFound } from './pages/NotFound'
 import { ScrollToTop } from './components/ScrollToTop'
@@ -11,23 +9,39 @@ import { ProductDetail } from './pages/ProductDetail/ProductDetail'
 import { CartProvider } from './context/CartProvider'
 import { ProductFormContainer } from './components/adminComponents/ProductFormContainer'
 import { Dashboard } from './pages/Admin'
+import { MainLayout } from './layouts/MainLayout'
+import { AdminLayout } from './layouts/AdminLayout'
+import { LoginFormContainer } from './components/authComponents/LoginFormContainer'
+import { ProtectedRoute } from './components/ProtectedRoute'
+import { AuthProvider } from './context/AuthProvider'
 
 function App() {
   return (
+    <AuthProvider>
     <CartProvider>
       <ScrollToTop />
-      <Header/>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/products' element={<Products/>}/>
-        <Route path='/products/:id' element={<ProductDetail/>}/>
-        <Route path='/categories' element={<Categories/>}/>
-        <Route path='/admin' element={<ProductFormContainer/>}/>
-        <Route path='/dashboard' element={<Dashboard/>}/>
-        <Route path='*' element={<NotFound/>}/>
+        <Route element={<MainLayout />}>
+          <Route path='/' element={<Home />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/products/:id' element={<ProductDetail />} />
+          <Route path='/categories' element={<Categories />} />
+        </Route>
+        <Route path='/admin' element={<AdminLayout />}>
+        <Route index element={<LoginFormContainer />} />
+
+          <Route path='/admin/dashboard' element={
+            <ProtectedRoute>
+                <Dashboard />
+          </ProtectedRoute>}>
+          </Route>
+
+        </Route>
+
+        <Route path='*' element={<NotFound />} />
       </Routes>
-      <Footer/>
     </CartProvider>
+    </AuthProvider>
   )
 }
 
